487f4f5c5b65c552a3b0ec312119616f50ec8627
Julien Marcou <julien.marcou.dev@gmail.com>
2018-10-02 17:00:04 +0200

    Factorize commands from popup

--- popup/pin-clip.js
+++ popup/pin-clip.js
@@ -1,9 +1,11 @@
+function queryActiveTab(command) {
+	browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
+		browser.tabs.sendMessage(tabs[0].id, {command: command});
+	});
+}
+
 document.addEventListener('click', (event) => {
-	if (event.target.classList.contains('toggle-overlay')) {
-
-		browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
-			browser.tabs.sendMessage(tabs[0].id, {command: 'toggle-overlay'});
-		});
-
+	if (event.target.dataset.command) {
+		queryActiveTab(event.target.dataset.command);
 	}
 });
